278da15280ee13ea15a954c023de0a7ee5cd4c0d
Andreas Barthel <andreas.barthel@outlook.com>
2023-10-04 06:23:14 +0000

    New file .space.kts

---
+++ .space.kts
@@ -0,0 +1,14 @@
+/**
+* JetBrains Space Automation
+* This Kotlin script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Hello World!") {
+    container(displayName = "Say Hello", image = "node:current-alpine") {
+      shellScript {
+        content = """
+        	npm run build
+        """
+    }
+}
