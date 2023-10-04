1cebf704565d5ceb3807f027ca42176e12af8810
Andreas Barthel <andreas.barthel@outlook.com>
2023-10-04 07:10:51 +0000

    more fixing

--- .space.kts
+++ .space.kts
@@ -8,6 +8,7 @@ job("Hello World!") {
     container(displayName = "Say Hello", image = "node:current-alpine") {
       shellScript {
         content = """
+        	npm install
         	./node_modules/@angular/cli/bin/ng.js build
         """
         }
